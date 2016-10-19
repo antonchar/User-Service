f246b80dfd585cf03b9980dcc24c8505a520ee85
Anton Charnamord <anton.charnamord@dev.com>
2016-10-19 23:44:35 +0300

    added bootstrap

---
+++ src/main/java/com/antonchar/webapp/StaticResourcesWebConfig.java
@@ -0,0 +1,14 @@
+package com.antonchar.webapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class StaticResourcesWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+    }
+}
